8cb98248ebe7968681e246fabe43d059c9035d20
pawurb <contact@pawelurbanek.com>
2020-11-30 00:22:28 +0100

    Generate docs

---
+++ generate-docs.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+dir=$(mktemp -d dist-docs.XXXXXX)
+trap 'rm -r "$dir"' EXIT
+
+cabal v2-haddock --builddir="$dir" --haddock-for-hackage --enable-doc
+
+cabal upload -d --publish $dir/*-docs.tar.gz
